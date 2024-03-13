aec6c98f658225610208a282ed4cdb489ae5556a
JOELTORCATO <a12022@ae4outubro.pt>
2024-03-13 15:18:52 +0000

    new commit. 13/03/2024

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 
+const port = 8080;
+
 // ROTA
 app.get('/home', function (req, res) {
   res.send('hello world');
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`server is running on ${port}`)
+});
