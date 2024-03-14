3030a4fd2c54a728876a8aadf8393a619119dcbc
JOELTORCATO <a12022@ae4outubro.pt>
2024-03-14 09:47:30 +0000

    config index, routes and controller.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
+const path = require('path')
+const routes = require('./routes/routes')
 const app = express()
-
 const port = 8080;
 
-// ROTA
-app.get('/home', function (req, res) {
-  res.send('hello world');
-});
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
 
-app.listen(port, () => {
-  console.log(`server is running on ${port}`)
-});
+app.listen(port, () =>
+  console.log(`server is running on http://localhost:${port}`)
+);
